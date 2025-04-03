1b7836dcf2a71c9360a1f3cad4832bb3f6c4d354
Ayushi Agrawal <aagrawal@dev2.wynton.ucsf.edu>
2025-04-02 23:08:22 -0700

    :runner:cellranger multi

---
+++ 01_cellranger_multi/cellranger_multi.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#$ -cwd
+#$ -o /gladstone/jain/boinformatics-collaboration/sb-1467-skyler-blume-isha-jain-snrnaseq-mm10-mar-2025/tmp/
+#$ -e /gladstone/jain/boinformatics-collaboration/sb-1467-skyler-blume-isha-jain-snrnaseq-mm10-mar-2025/tmp/
+#$ -pe smp 4
+#$ -l mem_free=50G
+#$ -l scratch=50G
+#$ -l h_rt=20:00:00
+#$ -j yes
+
+###############################################################################
+## Project ID: GB-SB-1467
+## Authors: Ayushi Agrawal
+##
+## Script Goal: run cellranger multi
+##
+## Expected command line arguments:
+##    args[1]: Name of the directory containing all cellranger-generated outputs
+##    args[2]: Path of the config file
+##    args[3]: Path of the output directory
+##
+## Example running this script on wynton:
+##    qsub cellranger_multi.sh \
+##	  gem_well_poll1 \
+##	  config.csv \
+##	  results/01_cellranger_multi
+###############################################################################
+
+#setup paths
+base_dir=/gladstone/jain/boinformatics-collaboration/sb-1467-skyler-blume-isha-jain-snrnaseq-mm10-mar-2025
+container_dir=$base_dir/assets/containers
+export APPTAINER_BINDPATH="$base_dir"
+
+#assign the argument values to variables
+sample_out_id=$1
+csv_path=$2
+outdir=$3
+
+#chnage working directory to output directory
+cd $outdir
+
+#run cellranger count
+apptainer exec $container_dir/cellranger_9.0.1.sif cellranger multi \
+	--id=$sample_out_id \
+	--csv=$csv_path
+
+
+# End-of-job summary, if running as a job
+[[ -n "$JOB_ID" ]] && qstat -j "$JOB_ID"
+
+
+## END 
---
+++ 01_cellranger_multi/step01_cellranger_multi_qsub.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#----------- define global variables
+base_dir=/gladstone/jain/boinformatics-collaboration/sb-1467-skyler-blume-isha-jain-snrnaseq-mm10-mar-2025
+
+# path to the cell ranger script
+cellranger_script=$base_dir/scripts/GB-SB-1467/01_cellranger_multi/cellranger_multi.sh
+
+# output directory
+outdir=$base_dir/results/01_cellranger_multi/
+
+# create the results output directory
+mkdir -p $outdir
+
+
+#----------- run cell ranger multi script for all config files
+# Find all CSV config files
+for file in $base_dir/scripts/GB-SB-1467/01_cellranger_multi/*.csv; do
+    # Extract the file name from the path
+    file_name=$(basename "$file")
+    
+    # Remove the _config.csv extension to get the base name
+    base_name="${file_name%.csv}"
+    sample_out_id="${base_name%_config}"
+
+    # print variables for logs
+    echo "Processing config file: $file, Output name: $sample_out_id, Output path: $outdir"
+
+    # run cellranger multi for current sample
+    qsub $cellranger_script $sample_out_id $file_name $outdir
+done
+
+
+################### END ###################
